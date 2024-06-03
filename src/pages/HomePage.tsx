718fc60e6726b3f44a2c63478751c4f05e327234
ZappyCrawdad <elitovarpalomino@gmail.com>
2024-06-02 23:15:26 -0600

    Imagen acompañando al Menu (final)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import CourseList from '../components/CourseList';
 import courses from '../data/courses';
+import image from '/img/Peralta-cursos-bot1.png'; // Ajusta la ruta según la ubicación real de tu imagen
 
 const HomePage: React.FC = () => {
   return (
     <div className="home-page">
       <h1>Lista de Cursos</h1>
+      <img src={image} alt="Imagen decorativa" className="decorative-image" />
       <CourseList courses={courses} emptyHeading="No hay cursos disponibles" />
     </div>
   );
